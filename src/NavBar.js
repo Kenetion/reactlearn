64633c2f7a93fc6686baf28debd70ffe64848e50
Kenetion <wojtekforum@gmail.com>
2023-03-04 13:25:50 +0100

    creating learning page and moving there all

--- src/NavBar.js
+++ src/NavBar.js
@@ -4,7 +4,8 @@ import { Link } from "react-router-dom"
 export const NavBar = () => {
     return <div>
     <Link to="/"> HOME </Link>
-    <Link to="/menu"> MENU </Link>
+    <Link to="/profile"> PROFILE </Link>
     <Link to="/contact"> CONTACT </Link>
+    <Link to="/learning"> LEARNING </Link>
   </div>
 }
