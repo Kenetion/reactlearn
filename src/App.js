d3a048d8d6fcee75f8115fbd38734127bad43540
Kenetion <wojtekforum@gmail.com>
2023-03-04 23:57:27 +0100

    added react-query

--- src/App.js
+++ src/App.js
@@ -7,14 +7,24 @@ import { NotSpecified } from './pages/notspecified';
 import { NavBar } from "./components/NavBar";
 import { Learning } from "./pages/learning";
 import { useState, createContext } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 export const AppContext = createContext();
 
 function App() {
+  const client = new QueryClient(
+    // example of when you don't want to refetch the data on tab change
+    //defaultOptions: {
+    // queries: {
+    //   refetchOnWindowFocus: false,
+    // },
+  // }
+  );
   const [username, setUsername] = useState ("");
 
   return (
     <div className="App">
+      <QueryClientProvider client={client}>
       <AppContext.Provider value={{username, setUsername}}>
       <Router>
         <NavBar />
@@ -27,6 +37,7 @@ function App() {
         </Routes>
       </Router>
       </AppContext.Provider>
+      </QueryClientProvider>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,17 @@
 import { useContext } from "react";
 import { AppContext } from "../App";
+import { useQuery } from "@tanstack/react-query";
+import Axios from "axios";
 
 export const Home = () => {
     const { username } = useContext(AppContext);
-    return <h1> This is the homepage and user is: { username }</h1>
+    const { data: catData, isLoading, refetch } = useQuery(["cat"], () => {
+      return  Axios.get("https://catfact.ninja/fact").then((res) => res.data);
+    });
+    if (isLoading) {
+        return <h1> Loading... </h1>
+    };
+    
+    return <h1> This is the homepage and user is: { username } <p>{ catData?.fact }</p>
+    <button onClick={refetch}> Update Data </button></h1>
 }
